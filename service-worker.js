ce5b89da30ecc5b7ff257202f9a974af36240136
Dax009 <38143460+LegitDax009@users.noreply.github.com>
2019-12-19 17:18:42 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+// CODELAB: Add list of files to cache here.
+const FILES_TO_CACHE = [
+  '/offline.html',
+];
+// CODELAB: Precache static resources here.
+evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+);
+// CODELAB: Precache static resources here.
+evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+);
+// CODELAB: Precache static resources here.
+evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+);
